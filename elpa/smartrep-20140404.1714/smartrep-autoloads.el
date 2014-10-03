3cbb41094ff910ccef1c411c7f9de4eeb5d23989
andyque <guanghui8827@126.com>
2014-10-03 12:43:06 +0800

    Default add bbatsov prelude to the git.

---
+++ elpa/smartrep-20140404.1714/smartrep-autoloads.el
@@ -0,0 +1,18 @@
+;;; smartrep-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("smartrep-pkg.el" "smartrep.el" "test-smartrep.el")
+;;;;;;  (21550 6550 41339 40000))
+
+;;;***
+
+(provide 'smartrep-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; smartrep-autoloads.el ends here
